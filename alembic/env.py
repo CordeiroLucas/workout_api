3bd91886b68a79e03e5b19925155b52e833f3805
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-08-22 09:59:11 -0300

    Fix: Funcionando basicos

--- alembic/env.py
+++ alembic/env.py
@@ -6,18 +6,15 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from workout_api.contrib.models import BaseModel
-from workout_api.contrib.repository.models import *
+from workoutapi.contrib.models import BaseModel
+from workoutapi.contrib.repository.models import *
 
 config = context.config
-
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-
 target_metadata = BaseModel.metadata
-
 
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
@@ -27,17 +24,15 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
-
     with context.begin_transaction():
         context.run_migrations()
 
-
-def do_run_migrations(connection: Connection) -> None: 
-    context.configure(connection=connection, target_metadata=target_metadata)
-    
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(
+        connection=connection, target_metadata=target_metadata
+    )
     with context.begin_transaction():
         context.run_migrations()
-
 
 async def run_async_migrations() -> None:
     connectable = async_engine_from_config(
@@ -48,10 +43,8 @@ async def run_async_migrations() -> None:
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
 
-
 def run_migrations_online() -> None:
     asyncio.run(run_async_migrations())
-
 
 if context.is_offline_mode():
     run_migrations_offline()
